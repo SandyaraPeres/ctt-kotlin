b55af431956304a443bdd936b412460c61ba7678
Sandyara Peres <40678077+SandyaraPeres@users.noreply.github.com>
2021-02-22 22:21:52 -0300

    Aula 05

---
+++ Aula_05/ProjetoAndroid/app/src/main/java/com/ctt/aula05/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ctt.aula05
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Todo nosso código irá aqui
+    }
+}
