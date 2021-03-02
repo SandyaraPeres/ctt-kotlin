2d0da7e971d36414d0f431608c1d9444ba13e0b5
Sandyara Peres <40678077+SandyaraPeres@users.noreply.github.com>
2021-03-01 22:22:06 -0300

    Aula 11 - Dúvidas & Scrum

---
+++ Aula_11/WhatZap/app/src/main/java/com/ctt/whatzap/ConversasAdapter.kt
@@ -0,0 +1,59 @@
+package com.ctt.whatzap
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ctt.whatzap.model.Contato
+
+//SEGUNDO PASSO A SER LIDO
+//Responsável por vincular os dados vindos da listaContatos ao XML item_contato (célula/item da lista)
+class ConversasAdapter(private val listaContatos: MutableList<Contato>) : RecyclerView.Adapter<ConversasAdapter.ConversaHolder>(){
+
+    //PRIMEIRO PASSO A SER LIDO
+    //Item final a ser visualizado e inserido na RecyclerView
+    class ConversaHolder(view: View) : RecyclerView.ViewHolder(view){
+        val nomeContato: TextView = view.findViewById(R.id.txtContato)
+        val ultimaMensagem: TextView = view.findViewById(R.id.txtMensagem)
+        val horaMensagem: TextView = view.findViewById(R.id.txtHorario)
+        val fotoContato: ImageView = view.findViewById(R.id.imgContato)
+    }
+
+    fun adicionarContato(novoContato: Contato){
+        //Adiciono um novo contato
+        listaContatos.add(novoContato)
+        MainActivity.listaConversEstatica.add(novoContato)
+        //NOTIFICO o Adapter que o dataset (fonte de dados) foi alterada e que ele precisa criar o "layout" dele
+        notifyDataSetChanged()
+    }
+
+//    fun fixarContato(contatinho: Contato){
+//  listaContatos.addAll()
+//        listaContatos.add(0, contatinho)
+//    }
+
+    //Equivalente ao onCreate da Activity, só que no caso, da nossa célula/item
+    //Da RecyclerView
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ConversaHolder {
+        val view = LayoutInflater.from(parent.context).
+        inflate(R.layout.item_contato, parent, false)
+
+        return ConversaHolder(view)
+    }
+
+    //Responsável por VINCULAR os dados com os componentes do xml
+    //Contato da lista = texto de contato do xml
+    override fun onBindViewHolder(holder: ConversaHolder, position: Int) {
+        listaContatos[position].imagem?.let{
+            holder.fotoContato.setImageBitmap(it)
+        }
+        holder.nomeContato.text = listaContatos[position].nome
+        holder.ultimaMensagem.text = listaContatos[position].ultimaMensagem
+        holder.horaMensagem.text = listaContatos[position].horarioMensagem
+    }
+
+    override fun getItemCount(): Int = listaContatos.size
+
+}
---
+++ Aula_11/WhatZap/app/src/main/java/com/ctt/whatzap/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.ctt.whatzap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.ctt.whatzap.model.Contato
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.jvm.internal.AdaptedFunctionReference
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        PASSOS PARA SE CRIAR UMA RECYCLERVIEW
+//                1. TENHA EM MÃOS OS SEUS DADOS (UMA MUTABLELISTOF POR EXEMPlO)
+//                2. JÁ TENHA CRIADO A RECYCLERVIEW EM SEU XML
+//                3. JÁ TENHA CRIADO O ITEM DA SUA LISTA EM XML
+//                4. CRIE O SEU ADAPTER, INDIQUE QUE O ITEM DA SUA EM XML É O ITEM DA RECYCLERVIEW VIA ONCREATEVIEWHOLDER
+//                5. CRIE SUA CLASSE VIEWHOLDER, ENCONTRE OS ITENS EM SEU XML COM FINDVIEWBYID
+//                6. VINCULE OS DADOS PASSADOS VIA ADAPTER AOS COMPONENTES VIA ONBINDVIEWHOLDER
+//                7. VINCULE O ADAPTER DA SUA RECYCLERVIEW A UMA INSTANCIA DA SUA CLASSE ADAPTER COM SEUS DADOS
+//                8. VINCULE UM GERENCIADOR DE LAYOUT (LAYOUT MANAGER) A SUA RECYCLERVIEW
+
+        //DADOS
+        val listaConversa = mutableListOf<Contato>(
+            Contato(imagem = null, nome = "Henrique", ultimaMensagem = "Vou pagar a breja!", horarioMensagem = "8:01 PM"),
+            Contato(imagem = null, nome = "Priscilla", ultimaMensagem = "Vou pagar a porção!!", horarioMensagem = "7:35 PM"),
+            Contato(imagem = null, nome="André", ultimaMensagem = "Gente, vamos beber para esquecer os B.O.", horarioMensagem = "7:05 PM")
+        )
+
+        val rvConversas = findViewById<RecyclerView>(R.id.listaContatos)
+        val addContato = findViewById<Button>(R.id.btnAddContato)
+
+        val adapterContato = ConversasAdapter(listaConversa)
+        rvConversas.adapter = adapterContato
+
+        addContato.setOnClickListener{
+            onBackPressed()
+            adapterContato.adicionarContato(
+                Contato(imagem = null, nome = "Ana", ultimaMensagem = "Vou desinstalar o IntelliJ! Misericórdia.", horarioMensagem = "7:00 PM")
+            )
+        }
+
+        //1. LinearLayout = Dispor os elementos em coluna única na vertical (padrão) OU horizontal
+        //2. GridLayout = Dispor os elementos em mais de uma coluna, como uma tabela
+        //3. StaggeredGridLayout = Dispor os elementos em mais de uma coluna, sem tamanho fixo
+        rvConversas.layoutManager = LinearLayoutManager(this)
+
+        //val exemploAchandoRecyclerViewComKotlin = listaContatos
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        //startActivity()
+    }
+
+    //MÉTODOS E ATRIBUTOS ESTÁTICOS
+    companion object{
+        //ESTÁTICOS
+        const val CHAVE_INTENT = "chaveIntent"
+        //val listaConversEstatica = mutableListOf<Contato>()
+    }
+}
